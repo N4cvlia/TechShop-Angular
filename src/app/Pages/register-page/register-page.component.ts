f710cc1370245ff7151f92beddf6aca0b112d7b0
Nacvlia <kaxa487@gmail.com>
2025-08-18 16:54:03 +0400

    Register form Validations updated

--- src/app/Pages/register-page/register-page.component.ts
+++ src/app/Pages/register-page/register-page.component.ts
@@ -2,10 +2,11 @@ import { Component} from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { ApiService } from '../../Services/api.service';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register-page',
-  imports: [RouterModule, ReactiveFormsModule],
+  imports: [RouterModule, ReactiveFormsModule, CommonModule],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.css'
 })
@@ -15,16 +16,56 @@ export class RegisterPageComponent {
   }
 
   public registerForm: FormGroup = new FormGroup({
-    firstName: new FormControl<string>("", [Validators.required, Validators.minLength(3)]),
-    lastName: new FormControl<string>("", [Validators.required, Validators.minLength(3)]),
-    age: new FormControl<number>(0, [Validators.required]),
-    email: new FormControl<string>("", [Validators.email, Validators.required]),
-    password: new FormControl<string>("", [Validators.required, Validators.minLength(5)]),
-    address: new FormControl<string>("", [Validators.required]),
-    phone: new FormControl<string>("+995", [Validators.required ]),
-    zipcode: new FormControl<string>("", [Validators.required]),
-    avatar: new FormControl<string>("", [Validators.required]),
-    gender: new FormControl<string>("", [Validators.required])
+    firstName: new FormControl<string>("", [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(50),
+      Validators.pattern(/^[A-Za-z]+$/)
+    ]),
+    lastName: new FormControl<string>("", [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(50),
+      Validators.pattern(/^[A-Za-z]+$/)
+    ]),
+    age: new FormControl<number | null>(null, [
+      Validators.required,
+      Validators.min(13),
+      Validators.max(120)
+    ]),
+    email: new FormControl<string>("", [
+      Validators.email,
+      Validators.required
+    ]),
+    password: new FormControl<string>("", [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.maxLength(64),
+      Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).+$/)
+    ]),
+    address: new FormControl<string>("", [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(200)
+    ]),
+    phone: new FormControl<string>("+995", [
+      Validators.required,
+      Validators.pattern(/^\+995\d{9}$/)
+    ]),
+    zipcode: new FormControl<string>("", [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(4),
+      Validators.pattern(/^[0-9]+$/)
+    ]),
+    avatar: new FormControl<string>("", [
+      Validators.required,
+      Validators.pattern(/^https?:\/\/.+/i)
+    ]),
+    gender: new FormControl<string>("", [
+      Validators.required,
+      Validators.pattern(/^(MALE|FEMALE|OTHER)$/i)
+    ])
   })
 
   public err: any;
