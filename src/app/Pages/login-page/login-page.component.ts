66d0d15025ff093383b3f3eb5328ea2111eaa9a3
Nacvlia <kaxa487@gmail.com>
2025-08-20 13:14:27 +0400

    Login Validations added

--- src/app/Pages/login-page/login-page.component.ts
+++ src/app/Pages/login-page/login-page.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ApiService } from '../../Services/api.service';
 import { CookieService } from 'ngx-cookie-service';
 import { Router, RouterModule } from '@angular/router';
 import { SubjectsService } from '../../Services/subjects.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login-page',
-  imports: [ReactiveFormsModule, RouterModule],
+  imports: [ReactiveFormsModule, RouterModule, CommonModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
@@ -17,11 +18,17 @@ export class LoginPageComponent {
   }
 
   public loginForm: FormGroup = new FormGroup({
-    email: new FormControl<string>(""),
-    password: new FormControl<string>("")
+    email: new FormControl<string>("", [
+      Validators.required,
+      Validators.email
+    ]),
+    password: new FormControl<string>("", [
+      Validators.required
+    ])
   })
 
-  public succ: boolean = false
+  public succ: boolean = false;
+  public err: boolean = false;
 
   login() {
     this.api.signIn(this.loginForm.value).subscribe({
@@ -33,12 +40,13 @@ export class LoginPageComponent {
           
         }, 3000);
         this.succ = true
+        this.err = false;
         this.subjects.getCart()
         this.subjects.renewPfp()
         this.subjects.notifyLogin();
       },
       error: (data:any) => {
-        console.log(data)
+        this.err = true;
       }
     })
   }
